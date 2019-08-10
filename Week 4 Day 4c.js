98fbbf5eeea251e75d6f6c198f19e7471ea6412f
Dipadana Putu <39789452+dipadana@users.noreply.github.com>
2019-08-10 21:47:55 +0700

    Update Week 4 Day 4c.js

--- Week 4 Day 4c.js
+++ Week 4 Day 4c.js
@@ -4,40 +4,34 @@ function countProfit(arr) {
     // Data barang bisa disimpan diluar fungsi atau didalam fungsi.
     let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
                         ['Baju Zoro', 500000, 2],
-                        ['Sweater Uniklooh', 175000, 1]
+                        ['Sweater Uniklooh', 175000, 1],
+                        ['Celana Boxer', 175000, 1]
                     ];
 
     // Mendeklarasikan beberapa variable yang dibutuhkan
-    var amountTemp = [listBarang[0][2],listBarang[1][2],listBarang[2][2]]; // Variable yang digunakan untuk menampung jumlah barang tersedia
-    var shoppersName = [[],[],[]]; // Variable yang digunakan untuk menampung tiap-tiap nama pembeli yang membeli barang
-    var profit = [0,0,0]; // Variable yang menampung jumlah uang yang didapatkan dari barang tersebut
-
+    var amountTemp = []; // Variable yang digunakan untuk menampung jumlah barang tersedia
+    var shoppersName = []; // Variable yang digunakan untuk menampung tiap-tiap nama pembeli yang membeli barang
+    var profit = []; // Variable yang menampung jumlah uang yang didapatkan dari barang tersebut
+    for(var y = 0; y < listBarang.length; y++){
+        amountTemp.push(listBarang[y][2]);
+        shoppersName.push([]);
+        profit.push(0);
+    }
+    
     // Dibagian ini dilakukan perhitungan pengelompokkan barang, nama pembeli, dan total profit yang didapat
     for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < listBarang.length; j++){
             // Dilakukan pengecekan, apakah barang yang ingin dibeli oleh pembeli, tersedia dijual oleh toko
-            if(arr[i].product == listBarang[0][0]){
+            if(arr[i].product == listBarang[j][0]){
                 // Jika barang tersedia untuk dijual, maka dicek lagi, apakah masih ada stock atau tidak
-                if(amountTemp[0] >= arr[i].amount){
+                if(amountTemp[j] >= arr[i].amount){
                     // Jika masih ada stock, value stock yang lama dikurang
-                    amountTemp[0] -= arr[i].amount;
+                    amountTemp[j] -= arr[i].amount;
                     // Lalu nama pembeli dimasukkan kedaftar pembeli
-                    shoppersName[0].push(arr[i].name);
+                    shoppersName[j].push(arr[i].name);
                     // Lalu profit dikalkulasi dan diupdate jika ada pembeli bertambah
-                    profit[0] = listBarang[0][1]*(listBarang[0][2]-amountTemp[0]);
+                    profit[j] = listBarang[j][1]*(listBarang[j][2]-amountTemp[j]);
                 }
-            }
-            else if(arr[i].product == listBarang[1][0]){
-                if(amountTemp[1] >= arr[i].amount){
-                    amountTemp[1] -= arr[i].amount;
-                    shoppersName[1].push(arr[i].name);
-                    profit[1] = listBarang[1][1]*(listBarang[1][2]-amountTemp[1]);
-                }
-            }
-            else if(arr[i].product == listBarang[2][0]){
-                if(amountTemp[2] >= arr[i].amount){
-                    amountTemp[2]-= arr[i].amount;
-                    shoppersName[2].push(arr[i].name);
-                    profit[2] = listBarang[2][1]*((listBarang[2][2]-amountTemp[2]));
             }
         }
     }
@@ -45,11 +39,10 @@ function countProfit(arr) {
     // Dibagian ini dilakukan pemprosesan pembuatan object,
     // untuk direturn sebagai output
     var objTemp = [];
-
+    // Jika array inputan kosong, maka yang direnturn adalah array kosong
     if(!arr[0]){
         return objTemp;
     }
-
     for(i = 0; i < listBarang.length; i++){
         var obj = {
             merk : listBarang[i][0],
@@ -60,7 +53,6 @@ function countProfit(arr) {
         objTemp[i] = obj;
     }
     return objTemp;
-    
 }
 
 
